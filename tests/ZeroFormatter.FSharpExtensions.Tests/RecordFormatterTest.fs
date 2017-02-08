7f8d38ffede5f212825f03c914ca8bc902bfeace
pocketberserker <pocketberserker@gmail.com>
2017-02-09 00:52:44 +0900

    add mutable record test

--- tests/ZeroFormatter.FSharpExtensions.Tests/RecordFormatterTest.fs
+++ tests/ZeroFormatter.FSharpExtensions.Tests/RecordFormatterTest.fs
@@ -20,8 +20,26 @@ module RecordFormatterTest =
   }
 
   [<Test>]
-  let record () =
+  let ``simple record`` () =
     let input = { MyProperty1 = 100; MyProperty2 = 99999999L; MyProperty3 = -123.43f }
     let xs = ZeroFormatterSerializer.Serialize(input)
     ZeroFormatterSerializer.Deserialize<MyRecord>(xs)
+    |> should equal input
+
+  [<ZeroFormattable>]
+  type MutableRecord = {
+    [<Index(0)>]
+    mutable Value: int
+  }
+
+  [<Test>]
+  let ``mutable record`` () =
+    let input = { Value = 100 }
+    let xs = ZeroFormatterSerializer.Serialize(input)
+    ZeroFormatterSerializer.Deserialize<MutableRecord>(xs)
+    |> should equal input
+
+    input.Value <- -1
+    let xs = ZeroFormatterSerializer.Serialize(input)
+    ZeroFormatterSerializer.Deserialize<MutableRecord>(xs)
     |> should equal input
